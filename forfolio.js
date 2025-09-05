92725e32387fd50cdd5efcc24360b07e929bb07e
TharakaVenkateswar <tharakavenkateswarpulikallu@gmail.com>
2025-09-05 17:10:35 +0530

    Initial commit

---
+++ forfolio.js
@@ -0,0 +1,9 @@
+const dark=function(){
+    document.querySelector("body").style.backgroundColor="black"
+    document.querySelector("body").style.color="white" 
+}
+const light=function(){
+    document.querySelector("body").style.backgroundColor="white"
+    document.querySelector("body").style.color="black"  
+}
+
