d9701368e631f0e627653409b00d16d57e5a0c19
TharakaVenkateswar <tharakavenkateswarpulikallu@gmail.com>
2025-11-07 14:19:39 +0530

    add

--- forfolio.js
+++ forfolio.js
@@ -1,9 +1,12 @@
 const dark=function(){
     document.querySelector("body").style.backgroundColor="black"
-    document.querySelector("body").style.color="white" 
+    document.querySelector("header").style.backgroundColor="black"
+    document.querySelector("body").style.color="rgb(44, 88, 90)" 
+    document.querySelector("section").style.animation=""
 }
 const light=function(){
     document.querySelector("body").style.backgroundColor="white"
+    document.querySelector("header").style.backgroundColor="#72ceeb"
     document.querySelector("body").style.color="black"  
 }
 
